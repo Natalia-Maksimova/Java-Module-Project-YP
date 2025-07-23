c38bf433faeade95b8d46c2956daa9d4ca310516
nataliaM <natashamaksimova2006@gmail.com>
2025-07-23 18:25:55 +0300

    commit

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -2,9 +2,9 @@ public class Race {
     static String leader;
 
     public static void counter(Car[] cars) {
-        int maxKm = 0;
+        int maxRun = 0;
         for (Car car : cars) {
-            if (car.speed * 24 > maxKm) leader = car.name;
+            if (car.speed * 24 > maxRun) leader = car.name;
         }
     }
 }
