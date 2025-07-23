521ea40a907d9ca53ed7324325e4e7ad5efabaed
nataliaM <natashamaksimova2006@gmail.com>
2025-07-23 18:23:04 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -5,36 +6,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Car[] cars = new Car[3];
         for (int i = 0; i < cars.length; i++) {
-            System.out.printf("— Введите название машины №%d: ", i+1);
+            System.out.printf("— Введите название машины №%d: ", i + 1);
             String name = scanner.next();
-            System.out.printf("— Введите скорость машины №%d:  ", i+1);
-            int speed = scanner.nextInt();
-            while (speed <= 0 | speed > 250) {
-                System.out.println("— Неправильная скорость");
-                System.out.printf("— Введите скорость машины №%d:  ", i+1);
-                speed = scanner.nextInt();
+            boolean validInput = false;
+            int speed = 0;
+
+            while (!validInput) {
+                try {
+                    System.out.printf("— Введите скорость машины №%d:  ", i + 1);
+                    speed = scanner.nextInt();
+                    if (speed <= 0 | speed > 250) throw new InputMismatchException();
+                    validInput = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("— Неправильная скорость");
+                    scanner.nextLine();
+                }
             }
             cars[i] = new Car(name, speed);
         }
         Race.counter(cars);
         System.out.printf("Самая быстрая машина: %s", Race.leader);
-    }
-}
-class Car {
-    int speed;
-    String name;
-    Car (String name, int speed) {
-        this.name = name;
-        this.speed = speed;
-    }
-}
-
-class Race {
-    static String leader;
-    public static void counter (Car[] cars) {
-        int maxKm = 0;
-        for (Car car : cars) {
-            if (car.speed * 24 > maxKm) leader = car.name;
-        }
+        scanner.close();
     }
 }
